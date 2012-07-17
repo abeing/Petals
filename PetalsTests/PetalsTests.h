54ed7296893ec5173cb64396da6c1aa9d2dbf9b2
Adam Miezianko <adam@theorylounge.org>
2012-07-16 23:30:37 -0700

    Initial Commit

---
+++ PetalsTests/PetalsTests.h
@@ -0,0 +1,13 @@
+//
+//  PetalsTests.h
+//  PetalsTests
+//
+//  Created by Adam Miezianko on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PetalsTests : SenTestCase
+
+@end
---
+++ PetalsTests/PetalsTests.m
@@ -0,0 +1,32 @@
+//
+//  PetalsTests.m
+//  PetalsTests
+//
+//  Created by Adam Miezianko on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PetalsTests.h"
+
+@implementation PetalsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PetalsTests");
+}
+
+@end
